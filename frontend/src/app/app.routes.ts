5b35a75e4994e07a3c91f9e26a0102ad7a56b363
Valentín Maschio <valecraftmax@gmail.com>
2026-09-21 01:22:03 -0300

    feat: login en Angular con interceptor y guard
    ...

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
+  { path: 'home', component: Home, canActivate: [authGuard] },
 ];
